db47f32d0ec32802aa8724207d0c0a39d5297a35
Kevin P. Barry <ta0kira@gmail.com>
2015-01-02 18:01:25 -0500

    minor updates to example

--- simple.cpp
+++ simple.cpp
@@ -15,10 +15,10 @@ int main() {
   //default: use 'rw_lock'
   typedef mutex_container <int> protected_int0;
 
-  //use a 'w_lock' instead
-  typedef mutex_container <int, w_lock> protected_int1;
+  //use 'r_lock' instead
+  typedef mutex_container <int, r_lock> protected_int1;
 
-  //the two types above share the same base class because the both protect 'int'
+  //the two types above share the same base class because they both protect 'int'
   typedef protected_int0::base base;
 
   //protected data
@@ -26,10 +26,15 @@ int main() {
   protected_int1 data1;
 
   //authorization object to prevent deadlocks (one per thread)
-  //NOTE: this will correspond to 'w_lock', since that's what 'protected_int1' uses
-  protected_int1::auth_type auth(protected_int1::new_auth());
+  //NOTE: this will correspond to 'rw_lock', since that's what 'protected_int0' uses
+  protected_int0::auth_type auth(protected_int0::new_auth());
   //make sure an authorization was provided
   assert(auth);
+
+  //alternatively, you can explicitly specify an authorization type
+  //NOTE: to use this, you'd pass '&auth2' to 'get_auth' or 'get_auth_const'
+  //(this might be useful if you want a thread to treat an object as read-only.)
+  lock_auth <r_lock> auth2;
 
   //proxy objects for accessing the protected data (use them like pointers)
   base::proxy       write;
@@ -38,8 +43,11 @@ int main() {
   //get a proxy, without deadlock prevention
   write = data0.get();
   assert(write); //(just for testing)
+  //write to the object
+  *write = 1;
   //release the lock
   write.clear();
+  assert(!write);
 
   //get a proxy, with deadlock prevention
   write = data0.get_auth(auth);
@@ -52,7 +60,7 @@ int main() {
   assert(read);
   read.clear();
 
-  //you can use the same proxy object with different lock types
+  //you can use the same proxy object with containers of the same base type
   read = data1.get_auth_const(auth);
   assert(read);
 
@@ -61,7 +69,7 @@ int main() {
     base::proxy write2 = data0.get_auth(auth);
     //(make sure the reason for failure is what we expected)
     if (!write2) assert(!auth->lock_allowed(false));
-  } //<-- 'write2' goes out of scope, which would unlock 'data0' of a lock was obtained
+  } //<-- 'write2' goes out of scope, which would unlock 'data0' if a lock was obtained
 
   {
     //copy the proxy object
